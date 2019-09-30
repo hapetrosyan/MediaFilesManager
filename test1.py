11626b1530b1badf97b17f03fb0d0dda81556360
hapetrosyan <hpetrosyan@questrade.com>
2019-09-29 22:41:56 -0400

    adding changes

--- test1.py
+++ test1.py
@@ -5,16 +5,19 @@ import hashlib
 import hash_utils as hu
 import sys
 
-# loc = "C:\\Users\\011868\\Pictures"
-loc = "/Users/hakob/Documents/ViberDownloads"
+
+loc = "C:\\Users\\011868\\Pictures"
+# loc = "/Users/hakob/Documents/ViberDownloads"
 # loc = "C:\\Q_C_Hakob"
 # text_file = open("C:\\Users\\011868\\Pictures\\Reports\\log.txt", "w")
 
 df = hu.get_location_files_hashes_dict(loc)
 
-# print(df)
+print(sys.version, sys.executable)
 
-print(df.groupby(['file_hash']).count())
+
+
+# print(df.groupby(['file_hash']).count())
 
 # print(fu.get_all_files_in_folder(loc))
 # print(sys.version)
---
+++ test3.py
@@ -0,0 +1,5 @@
+import matplotlib as m
+
+a = round(9.49)
+
+print(a)
