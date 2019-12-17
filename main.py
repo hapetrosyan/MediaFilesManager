7d514dc508e8b3ae0c6f4eb44be94acfe7fb59d8
Hakob <hapetrosyan@gmail.com>
2019-12-17 16:07:43 -0500

    polishing main

--- main.py
+++ main.py
@@ -7,37 +7,33 @@ import re
 
 # load csv file
 
-loc = '/media/hakob/Seagate Expansion Drive'
-os.remove('my_csv.csv')
-startime = datetime.now()
+# loc = '/media/hakob/Seagate Expansion Drive'
+# os.remove('my_csv.csv')
+# startime = datetime.now()
 
-for dirpath, dirnames, filenames in os.walk(loc):
-    # print(dirpath)
-    df = pd.DataFrame()
-    for filename in filenames:
-        # print(filename)
-        full_file_path = os.path.join(dirpath, filename)
-        df = df.append ( [[full_file_path, os.path.splitext(filename)[-1].lower(), hu.get_file_hash(full_file_path)]] )
-    df.to_csv('my_csv.csv', mode='a', header=False, index=False)
-    print(f'processing folder {dirpath}')
+# for dirpath, dirnames, filenames in os.walk(loc):
+#     # print(dirpath)
+#     df = pd.DataFrame()
+#     for filename in filenames:
+#         # print(filename)
+#         full_file_path = os.path.join(dirpath, filename)
+#         df = df.append ( [[full_file_path, os.path.splitext(filename)[-1].lower(), hu.get_file_hash(full_file_path)]] )
+#     df.to_csv('my_csv.csv', mode='a', header=False, index=False)
+#     print(f'processing folder {dirpath}')
 
-endtime = datetime.now()
-diff = endtime - startime
-print(diff)
+# endtime = datetime.now()
+# diff = endtime - startime
+# print(diff)
 
 # analyze csv
 
 df = pd.read_csv('my_csv.csv', names=['full_file_path', 'file_extension', 'file_hash'])
-df['full_file_path'] = df['full_file_path'].apply(lambda x: x.replace('/media/hakob/Seagate Expansion Drive/', ''))
-df['full_file_path'] = df['full_file_path'].str.lower()
+df['full_file_path'] = df['full_file_path'].apply(lambda x: x.replace('/media/hakob/Seagate Expansion Drive/', '')).str.lower() + '!'
 df['file_extension'] = df['file_extension'].str.lower()
-# df['path_words'] = df['full_file_path'].apply(lambda x: x.split('/')).apply(lambda x: x[1:])
-# df['path_words'] = df['full_file_path'].apply(lambda x: re.split('[ . / _  ,]' , x))
-
-df['full_file_path'] = df['full_file_path'] + '!'
 df_hash_paths_union = df.groupby('file_hash').agg('sum')
 df_hash_paths_union['paths_list'] = df_hash_paths_union['full_file_path'].apply(lambda x: re.split('[ . / _  , !]' , x)).apply(lambda x: list(dict.fromkeys(x)))
-# df_hash_paths_union.to_csv('path_words.csv')
+df_hash_paths_union.to_csv('path_words.csv')
+
 
 # finding all unique words to remove useless ones
 
