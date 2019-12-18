06c931b54cc28bef35caafb23039700b13645cd8
Hakob <hapetrosyan@gmail.com>
2019-12-17 22:34:49 -0500

    polishing funcs and note3

--- funcs.py
+++ funcs.py
@@ -14,17 +14,21 @@ ex_list = ['pana',
 'hd',
 'copied',
 'not',
-'to'
+'to',
+'xary'
 ]
 
 def remove_useless_words(lst):
 
     lst = [x for x in lst if x not in ex_list]
+    ex_list_1 = []
 
     for e in lst:
         if str(e)[-2:] == 'kb' or str(e)[:3] == 'dsc' or str(e)[:5] == 'image' or str(e)[:7] == 'picture' or str(e)[-5:] == 'nikon':
-            lst.remove(e)
-    
+            ex_list_1.append(e)
 
+    lst = [x for x in lst if x not in ex_list_1]
+
+    # IMPLEMENT THE CASE WITH p_______
 
     return lst
