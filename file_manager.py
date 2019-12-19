57b2c35fbdfe6d27d11ceb5a19cc1855db470387
Hakob <hapetrosyan@gmail.com>
2019-12-19 11:52:49 -0500

    add file manager.py

---
+++ file_manager.py
@@ -0,0 +1,121 @@
+import file_utils as fu
+import hash_utils as hu
+import pandas as pd
+from datetime import datetime
+import os
+import re
+import funcs
+import os
+import sys
+
+
+
+managed_files_folder = input('enter managed files folder: ')
+print ('Please enter operation type:')
+print ('1 - accept new files')
+print ('2 - shrink existing repo')
+operation_type_code =  input('enter the option : ')
+
+
+
+# args = sys.argv
+# age = 0
+# name = ''
+# city = ''
+
+# for i in range( len(args) ):
+#     if args[i] == '-managed files folder':
+#         age = args[i+1]
+#     elif args[i] == '-operation':
+#         name = args[i+1]
+#     elif args[i] == '-city':
+#         city = args[i+1]
+
+# print(age, name, city, aa)
+
+# load csv file
+
+if operation_type_code == '2':
+    all_files_hashes_csv_loc = managed_files_folder + '\\ServiceFiles\\all_files_hashes.csv'
+    print(all_files_hashes_csv_loc)
+    if os.path.exists(managed_files_folder + '\\ServiceFiles\\all_files_hashes.csv'):
+        os.remove(managed_files_folder + '\\ServiceFiles\\all_files_hashes.csv')
+
+    loc = managed_files_folder + '\\ClearRepo'
+
+    for dirpath, dirnames, filenames in os.walk(loc):
+        df = pd.DataFrame()
+        for filename in filenames:
+            full_file_path = os.path.join(dirpath, filename)
+            df = df.append ( [[full_file_path, os.path.splitext(filename)[-1].lower(), hu.get_file_hash(full_file_path)]] )
+        df.to_csv(all_files_hashes_csv_loc, mode='a', header=False, index=False)
+        print(f'processing folder {dirpath}')
+
+    df = pd.read_csv(all_files_hashes_csv_loc, names=['full_file_path', 'file_extension', 'file_hash'])
+
+    df['full_file_path'] = df['full_file_path'].apply(lambda x: x.replace('/media/hakob/Seagate Expansion Drive/', '')).str.lower() + '!'
+    df['file_extension'] = df['file_extension'].str.lower()
+    df_hash_paths_union = df.groupby('file_hash').agg('sum')
+    df_hash_paths_union['paths_list'] = df_hash_paths_union['full_file_path'].apply(lambda x: re.split('[ . / _  , !]' , x)).apply(lambda x: list(dict.fromkeys(x)))
+    df_hash_paths_union['desc_list'] = df_hash_paths_union['paths_list'].apply(funcs.remove_useless_words)
+    # df_hash_paths_union['desc_list'] = df['full_file_path'].apply(funcs.get_descriptions_list)
+    df_hash_paths_union.to_csv(managed_files_folder + '\\ServiceFiles\\path_words.csv')
+        
+"""
+loc = '/media/hakob/Seagate Expansion Drive'
+# os.remove('my_csv.csv')
+startime = datetime.now()
+
+for dirpath, dirnames, filenames in os.walk(loc):
+    df = pd.DataFrame()
+    for filename in filenames:
+        full_file_path = os.path.join(dirpath, filename)
+        df = df.append ( [[full_file_path, os.path.splitext(filename)[-1].lower(), hu.get_file_hash(full_file_path)]] )
+    df.to_csv('my_csv.csv', mode='a', header=False, index=False)
+    print(f'processing folder {dirpath}')
+
+endtime = datetime.now()
+diff = endtime - startime
+print(diff)
+"""
+
+# analyze csv
+'''
+df = pd.read_csv('my_csv.csv', names=['full_file_path', 'file_extension', 'file_hash'])
+df['full_file_path'] = df['full_file_path'].apply(lambda x: x.replace('/media/hakob/Seagate Expansion Drive/', '')).str.lower() + '!'
+df['file_extension'] = df['file_extension'].str.lower()
+df_hash_paths_union = df.groupby('file_hash').agg('sum')
+df_hash_paths_union['paths_list'] = df_hash_paths_union['full_file_path'].apply(lambda x: re.split('[ . / _  , !]' , x)).apply(lambda x: list(dict.fromkeys(x)))
+df_hash_paths_union['desc_list'] = df_hash_paths_union['paths_list'].apply(funcs.remove_useless_words)
+df_hash_paths_union.to_csv('path_words.csv')
+'''
+
+# deleting files
+'''
+df = pd.read_csv('my_csv.csv', names=['full_file_path', 'file_extension', 'file_hash'])
+df = df.sort_values('file_hash')
+df['is_copy'] = df['file_hash'].shift(1) == df['file_hash']
+files_to_del = df[df['is_copy'] == True]['full_file_path']
+for f in files_to_del:
+    if os.path.exists(f):
+        os.remove(f)
+    else:
+        print(f'The file {f} does not exist')
+'''
+
+
+
+
+
+
+
+# finding all unique words to remove useless ones
+"""
+s = {'jpg'}
+for r in df_hash_paths_union['paths_list']:
+    for elem in r:
+        s.add(elem)
+
+df_s = pd.DataFrame(s)
+df_s.to_csv('df_set.csv', index=False)
+"""
