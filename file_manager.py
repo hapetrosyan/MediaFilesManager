05290a7f60e03b00736d7d128127e1a098db2498
Hakob <hapetrosyan@gmail.com>
2019-12-19 23:47:28 -0500

    adding removal logic

--- file_manager.py
+++ file_manager.py
@@ -7,12 +7,12 @@ import re
 import funcs
 import os
 import sys
+import shutil
 
 
 
 
-
-base_folder = input('enter base folder: ')
+base_folder = '/home/hakob/temp/mfm' # input('enter base folder: ')
 
 managed_files_folder = base_folder + '/ManagedFilesFolder'
 guest_files = managed_files_folder + '/guest_files'
@@ -20,6 +20,7 @@ clean_repo = managed_files_folder + '/clean_folder'
 service_files = managed_files_folder + '/service_files' # make hidden
 tmp_csv = service_files + '/tmp_csv.csv'
 tmp_files_descr_list = service_files + '/tmp_files_descr_list.csv'
+clean_repo_file_list = service_files + '/clean_repo_file_list.csv'
 
 if not os.path.exists(managed_files_folder):
     os.mkdir(managed_files_folder)
@@ -29,6 +30,8 @@ if not os.path.exists(clean_repo):
     os.mkdir(clean_repo)
 if not os.path.exists(service_files):
     os.mkdir(service_files)
+if not os.path.exists(clean_repo_file_list):
+    shutil.copyfile('clean_repo_file_list.csv', clean_repo_file_list) 
 
 '''
 print ('Please enter operation type:')
@@ -56,7 +59,7 @@ operation_type_code =  input('enter the option : ')
 # load csv file
 
 tmp_csv = service_files + '/tmp_csv.csv'
-'''
+
 if os.path.exists(tmp_csv):
     os.remove(tmp_csv)
 
@@ -70,7 +73,7 @@ for dirpath, dirnames, filenames in os.walk(guest_files):
         df = df.append ( [[full_file_path, os.path.splitext(filename)[-1].lower(), hu.get_file_hash(full_file_path)]] )
     df.to_csv(tmp_csv, mode='a', header=False, index=False)
     print(f'processing folder {dirpath}')
-'''
+
 
 
 # analyze csv
@@ -79,10 +82,17 @@ df = pd.read_csv(tmp_csv, names=['full_file_path', 'file_extension', 'file_hash'
 # df['full_file_path'] = df['full_file_path'].apply(lambda x: x.replace('/media/hakob/Seagate Expansion Drive/', '')).str.lower() + '!'
 df['full_file_path'] = df['full_file_path'].apply(lambda x: x.replace(guest_files, '')).str.lower() + '!'
 df['file_extension'] = df['file_extension'].str.lower()
-df_hash_paths_union = df.groupby('file_hash').agg('sum')
+df_hash_paths_union = df.groupby('file_hash', as_index=False).agg('sum')
 df_hash_paths_union['paths_list'] = df_hash_paths_union['full_file_path'].apply(lambda x: re.split('[ . / _  , !]' , x)).apply(lambda x: list(dict.fromkeys(x)))
 df_hash_paths_union['desc_list'] = df_hash_paths_union['paths_list'].apply(funcs.remove_useless_words)
-df_hash_paths_union.to_csv(tmp_files_descr_list)
+df_hash_paths_union.to_csv(tmp_files_descr_list, index=False)
+
+df_guest_list = pd.read_csv(tmp_csv, names=['full_file_path', 'file_extension', 'file_hash'])
+df_guest_list = df_guest_list.sort_values('file_hash')
+df_guest_list['is_copy'] = df_guest_list['file_hash'].shift(1) == df_guest_list['file_hash']
+df_guest_list.to_csv(service_files + '/guest_csv.csv', index=False) # to remove
+
+
 
 
 # deleting files
